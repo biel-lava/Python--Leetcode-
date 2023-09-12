9bbe0c6700371e9b8f47a7d928c4372685ab1a0b
biel_lava <ralava@up.edu.ph>
2023-09-13 00:04:02 +0800

    Changed conditionals, added specific elif for case with "X" and "C" as the previous number

--- leetcode_13.py
+++ leetcode_13.py
@@ -6,21 +6,24 @@ Problem:
     - 
 '''
 
+# testing function
+
+
 roman_num = input("\nEnter roman numeral: ")
-equiv = {"I": 1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000 }
+equiv = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000 }
 sum = 0
-prev_num = ""
+prev_num = " "
 
 for current_num in roman_num.upper():
-    print(f"\nPrevious number: {prev_num}")
-    print(f"Current number: {current_num}")  
+    print(f"Current number: {equiv[current_num]}")  
     if current_num in ["X", "V", "L", "C", "D", "M"]:
-            if prev_num in ["I", "X", "C"]:
-                sum += equiv[current_num] - equiv[prev_num] #- equiv[prev_num]
-                #sum += current_sum
-    else:
-        sum += equiv[current_num]
+            if prev_num in ["I"]:
+                sum += equiv[current_num] - equiv[prev_num] 
+            elif prev_num in ["X", "C"]:
+                  sum += equiv[current_num] - equiv[prev_num] - equiv[prev_num]
+            else:
+                sum += equiv[current_num]
     print(f"Current sum: {sum}\n")    
     prev_num = current_num
     
-print("queen elsa ") 
+print(sum) 
