c85d60ad00cb266f092f6369469d611d450b1b9a
biel_lava <ralava@up.edu.ph>
2023-09-14 23:08:19 +0800

    Last attempt to solve problem 13

--- leetcode_13.py
+++ leetcode_13.py
@@ -5,7 +5,7 @@ Link: https://leetcode.com/problems/roman-to-integer/
 Problem:
     - 
 '''
-test_case = ["iv", "ix", "xl", "xc", "cd", "cm", "MCMXCIV"]
+test_case = ["iv", "ix", "xl", "xc", "cd", "cm", "MCMXCIV", "dcxxi"]
 
 def roman_converter(roman_number):
     equiv = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000 }
@@ -17,7 +17,7 @@ def roman_converter(roman_number):
                     if sum < 2:
                         sum = equiv[current_num] - equiv[prev_num]
                     else:
-                        sum -= equiv[current_num]  
+                        sum += equiv[current_num] - 2 
                 elif prev_num in ["X", "C"]:
                     sum += equiv[current_num] - equiv[prev_num] - equiv[prev_num]
                 else:
@@ -32,30 +32,4 @@ for roman in test_case:
     roman_converter(roman)
      
 
-'''
-roman_num = input("\nEnter roman numeral: ")
-equiv = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000 }
-sum = 0
-prev_num = " "
 
-for current_num in roman_num.upper():
-    print(f"Current number: {equiv[current_num]}")  
-    if current_num in ["X", "V", "L", "C", "D", "M"]:
-            if prev_num in ["I"]:
-                if sum < 1:
-                    sum = equiv[current_num] - equiv[prev_num]
-                else:
-                    sum -= equiv[current_num]  
-            elif prev_num in ["X", "C"]:
-                  sum += equiv[current_num] - equiv[prev_num] - equiv[prev_num]
-            else:
-                sum += equiv[current_num]
-    else:
-        sum += equiv[current_num]
-    
-    print(f"Current sum: {sum}\n")    
-    prev_num = current_num
-    
-print(sum) 
-
-'''
