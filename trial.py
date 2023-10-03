59ad002f39940772c35546cfd197a2f33b839396
biel_lava <ralava@up.edu.ph>
2023-10-03 21:21:38 +0800

    October trial

--- trial.py
+++ trial.py
@@ -1,3 +1,10 @@
-strs = ["flower", "flamingo", "flakes"]
+def oddNumbers(l, r):
+    for num in range(l, r+1):
+        if num % 2 == 0:
+            continue
+        else:
+            print(num) 
+        
 
-print(strs)
+oddNumbers(3, 9)
+
