2740d737f5d5e39bb8795a2c8b4e48ea14a12bef
biel_lava <ralava@up.edu.ph>
2023-10-07 22:42:30 +0800

    Trial only

--- trial.py
+++ trial.py
@@ -1,4 +1,7 @@
-list = ["elsa"]
-print(list)
-list.pop()
-print(list)
+numbers = [3, 6, 2, 4, 3, 6, 8, 9]
+
+for i in range(len(numbers)):
+    for j in range(i+1, len(numbers)):
+        if numbers[i] == numbers[j]:
+            print(f"{numbers[i]} is a duplicate")
+            break
