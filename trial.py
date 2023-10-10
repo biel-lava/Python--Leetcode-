ef70af9b20f806b93445e0ce4f568ad9f00425ad
biel_lava <ralava@up.edu.ph>
2023-10-11 01:28:10 +0800

    Created new file for FactSet tech exam practice

--- trial.py
+++ trial.py
@@ -1,7 +1,3 @@
-numbers = [3, 6, 2, 4, 3, 6, 8, 9]
+a,b = [1, 2, 3]
 
-for i in range(len(numbers)):
-    for j in range(i+1, len(numbers)):
-        if numbers[i] == numbers[j]:
-            print(f"{numbers[i]} is a duplicate")
-            break
+print(a,b)
